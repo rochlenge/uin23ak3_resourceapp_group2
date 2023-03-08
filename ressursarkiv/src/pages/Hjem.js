355ea964d5e5d0e80adfc9ab26a121512e627fee
tryggveh <97603758+tryggveh@users.noreply.github.com>
2023-03-08 22:41:06 +0100

    endret

--- ressursarkiv/src/pages/Hjem.js
+++ ressursarkiv/src/pages/Hjem.js
@@ -1,46 +1,44 @@
 import { Outlet, NavLink } from "react-router-dom";
-import React, { useState } from "react";
 
-function MyButton() {
-  const [isActive, setIsActive] = useState(false);
-
-  function handleClick(event) {
+function handleClick(event) {
     event.preventDefault();
-    setIsActive(true);
     event.currentTarget.querySelector("a").click();
   }
 
+const hjem = () => {
   return (
     <>
-      <div>
-        <header>
-          <h1 id="title">RESSURSARKIV</h1>
-        </header>
-        <nav>
-          <ul id="buttonbox">
-            <li className={`button ${isActive ? "active" : ""}`} onClick={handleClick}>
-              <NavLink to="/css" activeClassName="active">
-                CSS
-              </NavLink>
-            </li>
-            <li className="button" onClick={handleClick}>
-              <NavLink to="/html">HTML</NavLink>
-            </li>
-            <li className="button" onClick={handleClick}>
-              <NavLink to="/JavaScript">JavaScript</NavLink>
-            </li>
-            <li className="button" onClick={handleClick}>
-              <NavLink to="/React">React</NavLink>
-            </li>
-            <li className="button" onClick={handleClick}>
-              <NavLink to="/Sanity">Sanity</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Outlet />
+    <div>
+    <header>
+        <h1 id="title">RESSURSARKIV</h1>
+    </header>
+      <nav>
+      <ul id="buttonbox">
+        <li className="button" onClick={handleClick}>
+          <NavLink to="/css">CSS</NavLink>
+        </li>
+        <li className="button" onClick={handleClick}>
+            <NavLink to="/html">HTML</NavLink>
+          </li>
+          <li className="button" onClick={handleClick}>
+            <NavLink to="/JavaScript">JavaScript</NavLink>
+          </li>
+          <li className="button" onClick={handleClick}>
+            <NavLink to="/React">React</NavLink>
+          </li>
+          <li className="button" onClick={handleClick}>
+            <NavLink to="/Sanity">Sanity</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+
+
+      <Outlet />
       </div>
     </>
-  );
-}
 
-export default MyButton;
+  )
+};
+
+export default hjem;
